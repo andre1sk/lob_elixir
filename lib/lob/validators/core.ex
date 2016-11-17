463202fb0837bfd646e30ee0b7ca148be4e2368d
Andrei Skorokhod <andrei@Andreis-MacBook-Pro.local>
2016-11-17 15:11:17 -0500

    Core.Str max and min length validation

--- lib/lob/validators/core.ex
+++ lib/lob/validators/core.ex
@@ -4,12 +4,33 @@ end
 
 defmodule Lob.Validators.Core.Str do
   defstruct min: 0, max: nil, when: nil, not: nil, regex: nil, exists_in: nil
+
+  def validate_max(errors, val, max) do
+    len = String.length(val)
+    ok? = max == nil || len <= max
+    ok? && errors || [ "#{len} is bigger than max allowed #{max}" | errors]
+  end
+
+  def validate_min(errors, val, min) do
+    len = String.length(val)
+    ok? = min == nil || len >= min
+    ok? && errors || [ "#{len} is less than min allowed #{min}" | errors]
+  end
+
 end
 
 defimpl Lob.Validators.Core.Validate, for: Lob.Validators.Core.Str do
-  def validate(rule, val, data, errors) do
+  alias Lob.Validators.Core.Str
+
+  def validate(rule, val, data, errors) when is_binary(val) do
     errors
+    |> Str.validate_max(val, rule.max)
+    |> Str.validate_min(val, rule.min)
   end
+  def validate(_, val, _, errors) do
+    ["val must be a string got #{inspect val} instead" | errors]
+  end
+
 end
 
 defmodule Lob.Validators.Core.Map do
--- test/lob/validators/core_test.exs
+++ test/lob/validators/core_test.exs
@@ -14,4 +14,24 @@ defmodule Lob.Validators.CoreTest do
     assert res == []
   end
 
+  test "produces error if passed something other then binary" do
+    rule=%Str{}
+    res = validate(rule, %{}, %{}, [])
+    assert length(res) > 0
+  end
+
+  test "validates max length correctly" do
+    rule = %Str{max: 2}
+    assert validate(rule, "", %{}, []) == []
+    assert validate(rule, "z", %{}, []) == []
+    assert validate(rule, "abc", %{}, []) |> length > 0
+  end
+
+  test "validates min length correctly" do
+    rule = %Str{min: 3}
+    assert validate(rule, "", %{}, []) |> length > 0
+    assert validate(rule, "abc", %{}, []) == []
+    assert validate(rule, "abcd", %{}, []) == []
+  end
+
 end
